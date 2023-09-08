7634510087ddead885b1e6d556647a8d292aa3f4
Jean-Frederic Clere <jfclere@gmail.com>
2023-09-08 17:48:11 +0200

    Add a loop to check every 10 seconds.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ mod_kubernetes: mod_kubernetes.c
 	cc -c -I${HOME}/APR-1.7.x/include/apr-1 mod_kubernetes.c
 	cc -o mod_kubernetes mod_kubernetes.o ${HOME}/APR-1.7.x/lib/libapr-1.a
 image: mod_kubernetes Dockerfile
-	podman build --tag quay.io/jfclere/fedora:myhttpd -f ./Dockerfile
-	podman push quay.io/jfclere/fedora:myhttpd
+	podman build --tag quay.io/jfclere/fedora -f ./Dockerfile
+	podman push quay.io/jfclere/fedora
 run: image
 	podman run -it --name myhttpd --rm quay.io/jfclere/fedora:myhttpd mod_kubernetes google.com
--- mod_kubernetes.c
+++ mod_kubernetes.c
@@ -6,10 +6,29 @@
 #include "apr_inherit.h"
 #include "apr_network_io.h"
 
-int main(int argc, const char * const *argv)
+static void kubernetes_watchdog_func(const char *servicename, apr_pool_t *pool)
 {
     apr_status_t rv;
     apr_sockaddr_t *sa;
+    apr_pool_t *ptemp;
+    apr_pool_create(&ptemp, pool);
+    rv = apr_sockaddr_info_get(&sa, servicename, APR_UNSPEC, 8080, 0, ptemp);
+    if (rv == APR_SUCCESS) {
+        while(sa) {
+            char *ip_addr;
+            apr_sockaddr_ip_get(&ip_addr, sa);
+            printf("JFC find address %s %s %s\n", ip_addr, sa->hostname,  sa->servname);
+            sa = sa->next;
+        }
+    } else
+        printf("No pod for the service: %s\n", servicename);
+    fflush(stdout);
+    apr_pool_destroy(ptemp);
+
+}
+
+int main(int argc, const char * const *argv)
+{
     apr_pool_t *ptemp;
     const char *servicename;
 
@@ -21,15 +40,9 @@ int main(int argc, const char * const *argv)
 
     apr_initialize();
     apr_pool_create(&ptemp, NULL);
-    rv = apr_sockaddr_info_get(&sa, servicename, APR_UNSPEC, 8080, 0, ptemp);
-    if (rv == APR_SUCCESS) {
-        while(sa) {
-            char *ip_addr;
-            apr_sockaddr_ip_get(&ip_addr, sa);
-            printf("JFC find address %s %s %s\n", ip_addr, sa->hostname,  sa->servname);
-            sa = sa->next;
-        }
-    } else
-        printf("No pod for the service: %s\n", servicename);
+    for(;;) {
+        kubernetes_watchdog_func(servicename, ptemp);
+        apr_sleep(apr_time_from_sec(10));
+    }
     apr_terminate();
 }
